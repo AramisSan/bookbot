e24aeead30c083e75fa66996e43156558825d8ef
Asoch <aramissanchez0603@gmail.com>
2025-01-25 22:32:40 -0500

    added characther counter

--- main.py
+++ main.py
@@ -2,7 +2,16 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_text(book_path)
     num_words = get_num_words(text)
-    print(num_words)
+    num_char = get_num_char(text)
+    print(num_char)
+
+def get_num_char(text):
+    lowered_text = text.lower()
+    char_count = {}
+    for char in lowered_text:
+        if char.isalnum() or char.isspace():
+            char_count[char] = char_count.get(char, 0) + 1
+    return char_count
 
 def get_num_words(text):
     words = text.split()
